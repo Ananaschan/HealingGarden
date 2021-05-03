e56bbc9c601cf0d822ce5d3d48f34d24dbbd7532
ananas <ananas1024@qq.com>
2021-05-04 00:47:07 +0800

    0

--- src/main/java/healinggarden/controller/FoodController.java
+++ src/main/java/healinggarden/controller/FoodController.java
@@ -3,6 +3,7 @@ package healinggarden.controller;
 import healinggarden.pojo.Food;
 import healinggarden.service.food.FoodServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,11 +30,12 @@ public class FoodController {
     }
 
     @RequestMapping("/updateFood")
-    int updateFood(Food food) throws SQLException{
+    int updateFood(@RequestBody Food food) throws SQLException{
+        System.out.println(food.toString());
         return foodService.updateFood(food);
     }
 
-    @RequestMapping
+    @RequestMapping("/deleteFood")
     int deleteFood(int id) throws SQLException{
         return foodService.deleteFood(id);
     }
--- src/main/java/healinggarden/mapper/FoodMapper.java
+++ src/main/java/healinggarden/mapper/FoodMapper.java
@@ -1,9 +1,14 @@
 package healinggarden.mapper;
 
 import healinggarden.pojo.Food;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
+
 import java.sql.SQLException;
 import java.util.List;
 
+@Mapper
+@Component
 public interface FoodMapper {
     List<Food> getFoodList() throws SQLException;
     Food getFoodById(int id) throws SQLException;
